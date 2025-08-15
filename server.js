faa5fcf6caaa7ddf69474a79f647735c424ca1de
ghumansaab123 <ghumanhustler@gmail.com>
2025-08-15 22:13:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-curl ifconfig.me
+const axios = require('axios');
+
+async function getPublicIp() {
+  try {
+    // Fetch the public IP of the Render server using a service like 'ifconfig.me'
+    const response = await axios.get('https://ifconfig.me');
+    
+    console.log('Render Server IP Address:', response.data);
+  } catch (error) {
+    console.error('Error fetching IP address:', error);
+  }
+}
+
+// Call the function to fetch the IP address
+getPublicIp();
